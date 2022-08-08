9b655651bd2f273265810bfd882504deb4119c50
suren-g <82386446+suren-g@users.noreply.github.com>
2022-08-08 17:48:49 -0500

    initial project changes
    initial project changes

---
+++ Text/Text/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Text
+//
+//  Created by Suren R on 8/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        
+        Text("Hello ! surendhar").padding()
+            .background(Color.red).cornerRadius(16)
+            .padding(.all)
+            .background(Color.green).cornerRadius(15
+            )
+            .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+    }
+    
+}
---
+++ Text/Text/TextApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextApp.swift
+//  Text
+//
+//  Created by Suren R on 8/8/22.
+//
+
+import SwiftUI
+
+@main
+struct TextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
